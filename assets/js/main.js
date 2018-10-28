406d306e82f92b59bf833bb6e074d6304af0630a
ElhamZD <ez.dwedar@hotmail.com>
2018-10-28 23:33:02 +0200

    adding html code

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var scrollTop=0;
+	$(window).scroll(function(){
+		scrollTop = $(window).scrollTop();
+		if (scrollTop > 0){
+			$('.navbar').addClass ('scrollNav');
+		}else if(scrollTop == 0){
+			$('.navbar').removeClass('scrollNav');
+		}
+	});
+	$('#topBtn').click(function(){
+		$('html, body').animate({scrollTop:0}, '800')
+	});	
+});
